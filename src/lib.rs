f64c577ee089d1822b619a4739ed3dfd69080e10
octtoberrust <octtoberrust@outlook.com>
2024-09-05 21:08:53 -0500

    refactored test

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 //! src/lib.rs
 
 use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::dev::Server;
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
@@ -9,12 +10,13 @@ async fn health_check() -> HttpResponse {
 // We need to mark 'run' as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
-            App::new()
-                .route("/health_check", web::get().to(health_check))
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
         })
-        .bind("127.0.0.1::8000")?
-        .run()
-        .await
+        .bind("127.0.0.1:8000")?
+        .run();
+        //No .await here!
+        Ok(server)
 }
