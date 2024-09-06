8242e04d8b92a12e5978bfb9d274de17aa8a09da
octtoberrust <octtoberrust@outlook.com>
2024-09-06 08:14:29 -0500

    non-functional, saving remotely

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,5 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run()?.await
+    run()?await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,11 +5,17 @@
 //if we fail to perform the required setup we can just panic and crash 
 //all the things
 fn spawn_app() { 
-    let server = zero2prod::run().expect("Failed to bind address");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    //We retrieve the port assigne dto us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
     //Launch the server a background task
     //tokio::spawn returns a handle to the spawned future,
     //but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+
+    //We return the application address to the caller!
+    format!("http://127.0.0.1:{}", port)
 }
 
 // 'tokio::test' is the testing equivalent of 'tokio::main'.
@@ -21,14 +27,11 @@ fn spawn_app() {
 #[tokio::test]
 async fn health_check_works() {
     //No .await, no .expect
-    spawn_app();
-    //We need to bring in 'reqwest'
-    //to perform HTTP requests against our application
-    //but we have
-    let client = reqwest::Client::new();
+    let address = spawn_app();
+    let client = reqwest::Client::new9);
 
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get("&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
