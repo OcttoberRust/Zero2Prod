3691a60ddd623d30409863f4f20f07a98eae6e53
octtoberrust <octtoberrust@outlook.com>
2024-09-07 07:53:52 -0500

    updated tests and formatting

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,10 @@
 //! tests/health_check.rs
-
+use std::net::TcpListener;
 //No .await call, therefore no need for 'spawn_app' to be async now.
 //We are also running tests, so it is not worth it to propagate errors:
 //if we fail to perform the required setup we can just panic and crash 
 //all the things
-fn spawn_app() { 
+fn spawn_app() -> String { 
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     //We retrieve the port assigne dto us by the OS
     let port = listener.local_addr().unwrap().port();
@@ -28,10 +28,10 @@ fn spawn_app() {
 async fn health_check_works() {
     //No .await, no .expect
     let address = spawn_app();
-    let client = reqwest::Client::new9);
+    let client = reqwest::Client::new();
 
     let response = client
-        .get("&format!("{}/health_check", &address))
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
